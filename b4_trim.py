f843fb8784a9958f90b54fc5e2605930ad405e12
{haya_yu} <{flower87samo@gmail.com}git>
2022-12-30 00:59:56 +0900

    aaaiiiuu

--- b4_trim.py
+++ b4_trim.py
@@ -2,16 +2,16 @@ import pandas as pd
 import datetime as dt
 from decimal import *
 
-someone = ['watanabe/','suzuki/']#,'shigenawa/','maeda/','kobayasi/','kawasaki/','tamura/','tamaru/','nomura/','motoyama/','ota/','kawamura/','imahashi/','yashiro/','tabata/']
-file_name = ['n_puzzle1', 'n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','puzzle5-1','puzzle5-2','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2','iraira5-1','iraira5-2',]
+someone = ['tamura','suzuki']#,'shigenawa','maeda','kobayasi','kawasaki','watanabe','tamaru','nomura','motoyama','ota','kawamura','imahashi','yashiro','tabata']
+file_name = ['n_puzzle1', 'n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2']#'puzzle5-1','puzzle5-2','iraira5-1','iraira5-2',
 day = ['01', '02']
-gfi = ''
-
+df=pd.DataFrame(columns=file_name)
 for sm in someone:
-    for fn in file_name:
-        for dy in day:
+    for dy in day:
+        l_sm=[]
+        for fn in file_name:
             ################csvファイルから必要な時間の抽出#################
-            input_obj=pd.read_csv('data/' + 'kawamura'+ '01/' +'obj/'+ 'iraira1-1.csv',header=None)#ファイルの読み込み
+            input_obj=pd.read_csv('exp_data/' + sm+ dy +'/'+sm+dy+'_obj/'+ fn+'.csv',header=None)#ファイルの読み込み
 
             start=input_obj.iloc[0,0] #○○○○‐○○‐○○ ○○:○○:○○,○○○○
             start_dt=dt.datetime.strptime(start,'%Y-%m-%d %H:%M:%S.%f') #datetime型に
@@ -20,44 +20,15 @@ for sm in someone:
             end=input_obj.iloc[-1,0]
             end_dt=dt.datetime.strptime(end,'%Y-%m-%d %H:%M:%S.%f')
             end_unix=end_dt.timestamp() #UNIX時間に
-            #print("start=",start_unix)
-            #print("end  =",end_unix)
+            passtime=end_dt-start_dt
+            print(passtime)
+            l_sm.append(passtime)
             ##############################################################
+        print(l_sm)
+        df.loc[sm+dy]=l_sm
+        """
+        newline=pd.DataFrame([l_sm],columns=file_name,name=sm+dy)
+        pd.concat([df,newline])
+        """
 
-            input_mouse=pd.read_excel('data/' + 'kawamura'+ '01/' +'mousedata/'+'Data Export - '+'kawamura01/'+ 'kawamura'+'01 '+'iraira1-1.xlsx',index_col=None)#ファイルの読み込み
-            mouse_st=input_mouse.loc[0,'Recording date']+' '+input_mouse.loc[0,'Recording start time']
-            mouse_st_dt=dt.datetime.strptime(mouse_st,'%Y/%m/%d %H:%M:%S.%f')
-            mouse_st_unix=mouse_st_dt.timestamp()
-            #print(mouse_st_unix)
-            #text_df = input_mouse.drop(range(text_st_index[0] + 2))
-            #print(input_mouse)
-            for i in range(len(input_mouse)):
-                pass_time=input_mouse.loc[i,'Recording timestamp']
-                time=round(pass_time*0.000001,6)#秒に変換
-                #print(time)
-                now_unix=mouse_st_unix+time
-                if now_unix>start_unix:
-                    start_index=i
-                    break
-
-            for i in range(len(input_mouse)):
-                pass_time=input_mouse.loc[i,'Recording timestamp']
-                time=round(pass_time*0.000001,6)#秒に変換
-                now_unix=mouse_st_unix+time
-                if now_unix>end_unix:
-                    end_index=i
-                    break
-
-            input_mouse=input_mouse.drop(range(end_index+1,len(input_mouse)))
-            input_mouse= input_mouse.drop([i for i in range(start_index-1)])
-            mouse_df = input_mouse.loc[:,['Recording timestamp','Event','Mouse position X','Mouse position Y']]
-            mouse_df = mouse_df.reset_index(drop=True)
-            for i in range(len(mouse_df)):
-                t_stamp=mouse_df.iloc[i,0]
-                new_mouse_unix=round(t_stamp*0.000001,6)+mouse_st_unix
-                new_mouse_dt = dt.datetime.fromtimestamp(new_mouse_unix)
-                mouse_df.iloc[i,0]=new_mouse_dt
-
-  
-            mouse_df.to_csv('data/remove/' + 'kawamura_test'+ ' Data Export Trim.csv',  index=False)#ファイルに出力
-# print(df)
+df.to_csv('test.csv')
--- trim_test.py
+++ trim_test.py
@@ -3,7 +3,7 @@ import datetime as dt
 from decimal import *
 
 ################csvファイルから必要な時間の抽出#################
-input_obj=pd.read_csv('data/' + 'kawamura'+ '01/' +'obj/'+ 'iraira1-1.csv',header=None)#ファイルの読み込み
+input_obj=pd.read_csv('exp_data/' + 'imahashi'+ '01/' +'imahashi01_obj/'+ 'iraira1-1.csv',header=None)#ファイルの読み込み
 
 start=input_obj.iloc[0,0] #○○○○‐○○‐○○ ○○:○○:○○,○○○○
 start_dt=dt.datetime.strptime(start,'%Y-%m-%d %H:%M:%S.%f') #datetime型に
@@ -12,81 +12,69 @@ start_unix=start_dt.timestamp() #UNIX時間に
 end=input_obj.iloc[-1,0]
 end_dt=dt.datetime.strptime(end,'%Y-%m-%d %H:%M:%S.%f')
 end_unix=end_dt.timestamp() #UNIX時間に
+print(end_dt-start_dt)
 #print("start=",start_unix)
 #print("end  =",end_unix)
 ######################################################################
 
-###############################マウスデータ#############################
-input_mouse=pd.read_excel('data/' + 'kawamura'+ '01/' +'mousedata/'+'Data Export - '+'kawamura01/'+ 'kawamura'+'01 '+'iraira1-1.xlsx',index_col=None)#ファイルの読み込み
-mouse_st=input_mouse.loc[0,'Recording date']+' '+input_mouse.loc[0,'Recording start time']
-mouse_st_dt=dt.datetime.strptime(mouse_st,'%Y/%m/%d %H:%M:%S.%f')
-mouse_st_unix=mouse_st_dt.timestamp()
+###############################アイトラッカーアイトラッカーデータ#############################
+input_data=pd.read_excel('exp_data/' + 'imahashi'+ '01/' +'Data Export - '+'imahashi01/'+ 'imahashi'+'01 '+'iraira1-1.xlsx',index_col=None)#ファイルの読み込み
+recording_st=input_data.loc[0,'Recording date']+' '+input_data.loc[0,'Recording start time']
+recording_st_dt=dt.datetime.strptime(recording_st,'%Y/%m/%d %H:%M:%S.%f')
+recording_st_unix=recording_st_dt.timestamp()#レコーディング開始時刻
 
-for i in range(len(input_mouse)): #開始点探索
-    pass_time=input_mouse.loc[i,'Recording timestamp']
+for i in range(len(input_data)): #開始点探索
+    pass_time=input_data.loc[i,'Recording timestamp']
     time=round(pass_time*0.000001,6)#秒に変換
     #print(time)
-    now_unix=mouse_st_unix+time
+    now_unix=recording_st_unix+time
     if now_unix>start_unix:
         start_index=i
         break
 
-for i in range(len(input_mouse)):#終了点探索
-    pass_time=input_mouse.loc[i,'Recording timestamp']
+for i in range(start_index,len(input_data)):#終了点探索
+    pass_time=input_data.loc[i,'Recording timestamp']
     time=round(pass_time*0.000001,6)#秒に変換
-    now_unix=mouse_st_unix+time
+    now_unix=recording_st_unix+time
     if now_unix>end_unix:
         end_index=i
         break
 
-input_mouse=input_mouse.drop(range(end_index+1,len(input_mouse)))
-input_mouse= input_mouse.drop([i for i in range(start_index-1)])
-mouse_df = input_mouse.loc[:,['Recording timestamp','Event','Mouse position X','Mouse position Y']]
-mouse_df = mouse_df.reset_index(drop=True)
-for i in range(len(mouse_df)):
-    t_stamp=mouse_df.iloc[i,0]
-    new_mouse_unix=round(t_stamp*0.000001,6)+mouse_st_unix
-    new_mouse_dt = dt.datetime.fromtimestamp(new_mouse_unix)
-    mouse_df.iloc[i,0]=new_mouse_dt
 
-mouse_df.to_csv('data/kawamura01/remove/' + 'kawamura_iraira1mouse_test'+ ' Data Export Trim.csv',  index=False)#ファイルに出力
+input_data=input_data.drop(range(end_index,len(input_data)))#最後の余分な部分削除
+input_data= input_data.drop([i for i in range(start_index-1)])#最初の余分な部分削除
+input_data = input_data.loc[:,['Recording timestamp','Event','Sensor','Gaze point X','Gaze point Y','Validity left','Validity right','Mouse position X','Mouse position Y']]
+input_data = input_data.reset_index(drop=True)#インデックス振り直し
+
+for i in range(len(input_data)):#時間の書き換え
+    t_stamp=input_data.iloc[i,0]
+    new_unix=round(t_stamp*0.000001,6)+recording_st_unix
+    new_dt = dt.datetime.fromtimestamp(new_unix)
+    input_data.iloc[i,0]=new_dt
+
+
+eyetracker_df=input_data#コピー
+drop_index=eyetracker_df.index[eyetracker_df['Sensor']!='Eye Tracker']
+eyetracker_df=eyetracker_df.drop(drop_index)
+eyetracker_df = eyetracker_df.reset_index(drop=True)
+eyetracker_df = eyetracker_df.loc[:,['Recording timestamp','Event','Sensor','Gaze point X','Gaze point Y','Validity left','Validity right']]
+eyetracker_df.to_csv('exp_data/imahashi01/remove/' + 'imahashi01_'+ 'eye_all.csv',  index=False)#視線データ（invalidあり）ファイルに出力
+
+drop_index=eyetracker_df.index[(eyetracker_df['Validity left']=='Invalid') & (eyetracker_df['Validity right']=='Invalid')]
+eyetracker_df=eyetracker_df.drop(drop_index)
+eyetracker_df = eyetracker_df.reset_index(drop=True)
+eyetracker_df.to_csv('exp_data/imahashi01/remove/' + 'imahashi01_'+ 'eye_valid.csv',  index=False)#ファイルに出力（invalidなし）
+
+################マウスデータ出力#######################
+drop_index=input_data.index[input_data['Sensor']!='Mouse']
+input_data=input_data.drop(drop_index)
+input_data = input_data.reset_index(drop=True)
+input_data = input_data.loc[:,['Recording timestamp','Event','Sensor','Mouse position X','Mouse position Y']]
+input_data.to_csv('exp_data/imahashi01/remove/' + 'imahashi01_'+ 'mouse.csv',  index=False)#ファイルに出力
+
+
+#mouse_df.to_csv('data/kawamura01/remove/' + 'kawamura_iraira1mouse_test'+ ' Data Export Trim.csv',  index=False)#ファイルに出力
 ######################################################################
-
-##############################視線データ###############################
-input_eye=pd.read_excel('data/' + 'kawamura'+ '01/' +'eyedata/'+'Data Export - '+'kawamura01/'+ 'kawamura'+'01 '+'iraira1-1.xlsx',index_col=None)#ファイルの読み込み
-eye_st=input_eye.loc[0,'Recording date']+' '+input_eye.loc[0,'Recording start time']#rec開始時間
-eye_st_dt=dt.datetime.strptime(eye_st,'%Y/%m/%d %H:%M:%S.%f')#datetime型に
-eye_st_unix=eye_st_dt.timestamp()#unixに
-
-for i in range(len(input_eye)): #開始点探索
-    pass_time=input_eye.loc[i,'Recording timestamp']
-    time=round(pass_time*0.000001,6)#秒に変換
-    now_unix=eye_st_unix+time
-    if now_unix>start_unix:
-        start_index=i
-        break
-
-for i in range(len(input_eye)):#終了点探索
-    pass_time=input_eye.loc[i,'Recording timestamp']
-    time=round(pass_time*0.000001,6)#秒に変換
-    now_unix=eye_st_unix+time
-    if now_unix>end_unix:
-        end_index=i
-        break
-
-input_eye=input_eye.drop(range(end_index+1,len(input_eye)))
-input_eye=input_eye.drop([i for i in range(start_index-1)])
-eye_df =input_eye.loc[:,['Recording timestamp','Event','Gaze point X','Gaze point Y','Validity left','Validity right']]
-eye_df = eye_df.reset_index(drop=True)
-for i in range(len(eye_df)):#recording time stampをdatetimeに変換
-    t_stamp=eye_df.iloc[i,0]
-    new_eye_unix=round(t_stamp*0.000001,6)+eye_st_unix
-    new_eye_dt = dt.datetime.fromtimestamp(new_eye_unix)
-    eye_df.iloc[i,0]=new_eye_dt
-
-  
-eye_df.to_csv('data/kawamura01/remove/' + 'kawamura_iraira1eye_test'+ ' Data Export Trim.csv',  index=False)#ファイルに出力
-#######################################################################
 
 """
 print(round(input_mouse.iloc[start_index-1,0]*0.000001,6)+mouse_st_unix)
