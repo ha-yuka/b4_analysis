33ab9ba6107837472cab14519b5124b4e76f9fcc
{haya_yu} <{flower87samo@gmail.com}git>
2023-01-24 20:09:00 +0900

    s

--- 6_kakudo.py
+++ 6_kakudo.py
@@ -7,13 +7,13 @@ import datetime as dt
 
 #クリック直後の視線とマウスの動く角度
 
-someone = ['imahashi']#'tamura',,'watanabe',,,'kawamura','kawasaki','kobayashi','maeda','motoyama','tamaru','nomura','ota','shigenawa','suzuki','tabata','yashiro'
-file_name = ['n_iraira1','n_iraira2','n_iraira3','n_iraira4','n_iraira5','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2','iraira5-1','iraira5-2']
-#file_name=['n_puzzle1', 'n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','puzzle5-1','puzzle5-2']
+someone = ['watanabe','kawamura','kawasaki','kobayashi','maeda','motoyama','tamaru','nomura','ota','shigenawa','suzuki','tabata','yashiro','imahashi']#'tamura',
+#file_name = ['n_iraira1','n_iraira2','n_iraira3','n_iraira4','n_iraira5','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2','iraira5-1','iraira5-2']
+file_name=['n_puzzle1', 'n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','puzzle5-1','puzzle5-2']
 day = ['02']#'01',
 pre=[]
 post=[]
-after=15
+after=16
 
 kakudo=[]
 #========================アンケート結果読み込み=====================#
@@ -23,14 +23,6 @@ task02=pd.read_excel('task02.xlsx',index_col=None)#ファイルの読み込み
 for sm in someone:
     for dy in day:
         for fn in file_name:
-            #========================接触回数の確認======================#
-            input_touch=pd.read_csv('exp_data/'+sm+dy+'/'+sm+dy+'_obj/touch.csv', index_col=None,header=None)#接触回数データ読み込み
-            index=(input_touch.index[input_touch[1]==fn])[0]
-            #print(index)
-            num=input_touch.loc[index,2]
-            if num==0:
-                break
-                #kakudo.append(0)
             #=================アンケートの結果=================
             mouse_start=[]
             mouse_end=[]
@@ -39,23 +31,16 @@ for sm in someone:
             pop_index=[]
             k_list=[]
             start_time=0
-            if dy=='01':
-                index=(task01.index[task01['被験者']==sm])[0]
-                pre_q=fn+'_pre'
-                post_q=fn+'_post'
-                pre.append(task01.loc[index,pre_q])
-                post.append(task01.loc[index,post_q])
-            else:
-                index=(task02.index[task02['被験者']==sm])[0]
-                pre_q=fn+'_pre'
-                post_q=fn+'_post'
-                pre.append(task02.loc[index,pre_q])
-                post.append(task02.loc[index,post_q])
-            #print(index)
+            #a#,b,c,d,e,f,g,h=0
             #================================================
             input_mouse=pd.read_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'mouse.csv', index_col=None)#マウスデータ読み込み
+            input_mouse=input_mouse.fillna('')
             click_index = input_mouse.index[(input_mouse['Event value'] == 'Down, Left')].tolist()#クリックのインデックス取得
+            
+            # mevent_index = input_mouse.index[(input_mouse['Event value'] != 'Down, Left') & (input_mouse['Event value'] != 'Down, Left')].tolist()#マウスイベントインデックス取得
             input_eye=pd.read_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'lerp.csv', index_col=None)#視線データ読み込み
+            if len(click_index)==0:
+                break
             if len(click_index)>=2:
                 for i in range(1,len(click_index)):
                     if click_index[i]-click_index[i-1]<=after:
@@ -65,14 +50,20 @@ for sm in someone:
 
             if len(click_index)>=2:
                 for i in range(0,len(click_index)-1):
-                    a = input_mouse.at[input_mouse.index[i], 'Mouse position X']  # 視点のx座標
-                    b = input_mouse.at[input_mouse.index[i], 'Mouse position Y']  # 視点のy座標
+                    a = input_mouse.at[input_mouse.index[click_index[i]+1], 'Mouse position X']  # 視点のx座標
+                    b = input_mouse.at[input_mouse.index[click_index[i]+1], 'Mouse position Y']  # 視点のy座標
                     mouse_start=[a, b]
-                    c = input_mouse.at[input_mouse.index[i+after], 'Mouse position X']  # 視点のx座標
-                    d = input_mouse.at[input_mouse.index[i+after], 'Mouse position Y']  # 視点のy座標
+                    if (input_mouse.loc[click_index[i]+after,'Event'] == ''):
+                        #print("none")
+                        c = input_mouse.at[input_mouse.index[click_index[i]+after], 'Mouse position X']  # 視点のx座標
+                        d = input_mouse.at[input_mouse.index[click_index[i]+after], 'Mouse position Y']  # 視点のy座標
+                    else:
+                        c = input_mouse.at[input_mouse.index[click_index[i]-1+after], 'Mouse position X']  # 視点のx座標
+                        d = input_mouse.at[input_mouse.index[click_index[i]-1+after], 'Mouse position Y']  # 視点のy座標
                     mouse_end=[c, d]
-                    mouse_vec=np.array([c-a,d-b])
-                    sac_start = input_mouse.at[input_mouse.index[click_index[i]], 'Recording timestamp']  # サッカード開始時刻
+                    mouse_vec=np.array([c-a,d-b])                                            
+
+                    sac_start = input_mouse.at[input_mouse.index[click_index[i]+1], 'Recording timestamp']  # クリック点
                     ind=input_eye['Recording timestamp'].sub(sac_start).abs().idxmin()#最も近い時間の値
                     e = input_eye.at[input_eye.index[ind], 'Gaze point X']  # 視点のx座標
                     f = input_eye.at[input_eye.index[ind], 'Gaze point Y']  # 視点のy座標
@@ -81,22 +72,29 @@ for sm in someone:
                     h = input_eye.at[input_eye.index[ind+after], 'Gaze point Y']  # 視点のy座標
                     eye_end=[g, h]
                     eye_vec=np.array([g-e,h-f])
-                    naiseki=np.inner(eye_vec,mouse_vec)
-                    theta=np.arccos(naiseki/(np.linalg.norm(eye_vec)*np.linalg.norm(mouse_vec)))
-                    k_list.append(theta)
+
+                    naiseki=np.dot(eye_vec,mouse_vec)
+                    l_eye=(np.linalg.norm(eye_vec))
+                    l_mouse=(np.linalg.norm(mouse_vec))
+
+                    if l_eye!=0 and l_mouse!=0:#0割り防止
+                        theta=np.arccos(naiseki/(l_eye*l_mouse))
+                        #print(fn,click_index[i]+after,mouse_start,mouse_end,mouse_vec,naiseki,theta)
+                        k_list.append(theta)
+                    else:
+                        break
     
-                    # print("=====================================")
-                    # print(eye_start)
-                    # print(eye_end)
-                    #print(eye_vec)
-
-
             if click_index[-1]+after<len(input_mouse)-1:
-                    a = input_mouse.at[input_mouse.index[click_index[-1]], 'Mouse position X']  # 視点のx座標
-                    b = input_mouse.at[input_mouse.index[click_index[-1]], 'Mouse position Y']  # 視点のy座標
+                    a = input_mouse.at[input_mouse.index[click_index[-1]+1], 'Mouse position X']  # 視点のx座標
+                    b = input_mouse.at[input_mouse.index[click_index[-1]+1], 'Mouse position Y']  # 視点のy座標
                     mouse_start=[a, b]
-                    c = input_mouse.at[input_mouse.index[click_index[-1]+after], 'Mouse position X']  # 視点のx座標
-                    d = input_mouse.at[input_mouse.index[click_index[-1]+after], 'Mouse position Y']  # 視点のy座標
+                    if (input_mouse.loc[click_index[-1]+after,'Event'] == ''):
+                        #print("none")
+                        c = input_mouse.at[input_mouse.index[click_index[-1]+after], 'Mouse position X']  # 視点のx座標
+                        d = input_mouse.at[input_mouse.index[click_index[-1]+after], 'Mouse position Y']  # 視点のy座標
+                    else:
+                        c = input_mouse.at[input_mouse.index[click_index[-1]-1+after], 'Mouse position X']  # 視点のx座標
+                        d = input_mouse.at[input_mouse.index[click_index[-1]-1+after], 'Mouse position Y']  # 視点のy座標
                     mouse_end=[c, d]
                     mouse_vec=np.array([c-a,d-b])
                     sac_start = input_mouse.at[input_mouse.index[click_index[-1]], 'Recording timestamp']  # サッカード開始時刻
@@ -111,9 +109,22 @@ for sm in someone:
                     naiseki=np.inner(eye_vec,mouse_vec)
                     theta=np.arccos(naiseki/(np.linalg.norm(eye_vec)*np.linalg.norm(mouse_vec)))
                     k_list.append(theta)
+            #print(sum(k_list)/len(k_list))
             kakudo.append(sum(k_list)/len(k_list))
-    
 
+            if dy=='01':
+                index=(task01.index[task01['被験者']==sm])[0]
+                pre_q=fn+'_pre'
+                post_q=fn+'_post'
+                pre.append(task01.loc[index,pre_q])
+                post.append(task01.loc[index,post_q])
+            else:
+                index=(task02.index[task02['被験者']==sm])[0]
+                pre_q=fn+'_pre'
+                post_q=fn+'_post'
+                pre.append(task02.loc[index,pre_q])
+                post.append(task02.loc[index,post_q])
+            print(sm,fn)
             
 print("-------------------pre--------------------")
 plt.scatter(kakudo,pre)
@@ -123,8 +134,6 @@ plt.ylim(0,100)
 # X2 = [[x] for x in kakudo]
 # clf.fit(X2, pre) # 予測モデルを作成
 # plt.plot(X2, clf.predict(X2))
-#ax.text(19.0, 0.6, '$ R^{2} $=' + str(round(r2_lin, 4)))
-#plt.plot(df_x, y_lin_fit, color = '#000000', linewidth=0.5)
 plt.xlabel("角度", fontname="MS Gothic")
 plt.ylabel("事前自己効力感", fontname="MS Gothic")
 plt.show()
@@ -142,7 +151,7 @@ plt.ylim(0,100)
 # clf = linear_model.LinearRegression()
 # X2 = [[x] for x in kakudo]
 # clf.fit(X2, post) # 予測モデルを作成
-# plt.plot(X2, clf.predict(X2))
+#plt.plot(X2, clf.predict(X2))
 plt.xlabel("角度", fontname="MS Gothic")
 plt.ylabel("事後自己効力感", fontname="MS Gothic")
 plt.show()
--- datetime_to_unix.py
+++ datetime_to_unix.py
@@ -2,22 +2,33 @@ import pandas as pd
 import datetime as dt
 from decimal import *
 
-someone = ['tamura']#''imahashi',,'kawamura''kawasaki','kobayashi','maeda','motoyama','nomura','ota','shigenawa','suzuki','tabata','tamaru','watanabe','yashiro',,
-file_name = ['puzzle5-1','puzzle5-2','iraira5-1','iraira5-2']#',,,'n_puzzle1', 'n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','n_iraira1','n_iraira2','n_iraira3','n_iraira4','n_iraira5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2'
+someone =['ota']#,'tamura',,,'imahashi','kawamura',,'kobayashi','maeda','motoyama','nomura',,'shigenawa','suzuki','tabata','tamaru',,,,,,,,,,,,,,,,,,,,,'watanabe','yashiro''ota'
+file_name = ['n_puzzle1','n_puzzle2', 'n_puzzle3','n_puzzle4','n_puzzle5','puzzle1-1','puzzle1-2','puzzle2-1','puzzle2-2','puzzle3-1','puzzle3-2','puzzle4-1','puzzle4-2','puzzle5-1','puzzle5-2']#,,,,'n_iraira1','n_iraira2','n_iraira3','n_iraira4','n_iraira5','iraira1-1','iraira1-2','iraira2-1','iraira2-2','iraira3-1','iraira3-2','iraira4-1','iraira4-2','iraira5-1','iraira5-2'
 day = ['02']#,,,'02'
 for sm in someone:
     for dy in day:
         l_sm=[]
         for fn in file_name:            
             ###############################アイトラッカーデータ#############################
-            input_mouse=pd.read_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'mouse.csv', index_col=None)#マウスデータ読み込み
+            # input_obj=pd.read_csv('exp_data/'+sm+dy+'/'+sm+dy+'_obj/' + fn+ '.csv', header=None,index_col=None)#オブジェクト操作データ
             
-            for i in range(len(input_mouse)):#時間の書き換え
-                t_stamp=input_mouse.iloc[i,0]
-                start_dt=dt.datetime.strptime(t_stamp,'%Y-%m-%d %H:%M:%S.%f') #datetime型に
-                start_unix=start_dt.timestamp() #UNIX時間に
-                input_mouse.iloc[i,0]=start_unix
-            input_mouse.to_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'mouse.csv',  index=False)#ファイルに出力
+            # for i in range(len(input_obj)):#時間の書き換え
+            #     t_stamp=input_obj.iloc[i,0]
+            #     start_dt=dt.datetime.strptime(t_stamp,'%Y-%m-%d %H:%M:%S.%f') #datetime型に
+            #     start_unix=start_dt.timestamp() #UNIX時間に
+            #     input_obj.iloc[i,0]=start_unix
+            # input_obj.columns=['Recording timestamp','Position X','Position Y']
+            # input_obj.to_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'obj.csv',  index=False)#ファイルに出力
+            # print(sm,dy,fn)
+
+            # input_eye=pd.read_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'mouse.csv', index_col=None)#マウスデータ読み込み
+            # for i in range(len(input_eye)):#時間の書き換え
+            #     t_stamp=input_eye.iloc[i,0]
+            #     #print(t_stamp)
+            #     start_dt=dt.datetime.strptime(t_stamp,'%Y-%m-%d %H:%M:%S.%f') #datetime型に
+            #     start_unix=start_dt.timestamp() #UNIX時間に
+            #     input_eye.iloc[i,0]=start_unix
+            # input_eye.to_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'mouse.csv',  index=False)#ファイルに出力
 
             input_eye=pd.read_csv('exp_data/'+sm+dy+'/remove/' + fn+'_'+ 'eye_all.csv', index_col=None)#マウスデータ読み込み
             for i in range(len(input_eye)):#時間の書き換え
